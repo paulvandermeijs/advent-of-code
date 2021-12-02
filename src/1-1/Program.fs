24a5cd1cfdb30ea2f3cd32212b5fd5ca60953506
Paul van der Meijs <vandermeijs@redkiwi.nl>
2021-12-02 08:06:13 +0100

    Add day 1-1

---
+++ src/1-1/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+let rec count last total =
+    match box last, box (Console.ReadLine()) with
+    | (:? string as a), (:? string as b) -> count b (total + Convert.ToInt32((int) b > (int) a))
+    | _ -> total
+
+printfn "%i" (count (Console.ReadLine()) 0)
